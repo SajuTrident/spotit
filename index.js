c5b3b0caa400954a8b994c99a9f143cbb3a9e409
Sajad pp <96676224+SajuTrident@users.noreply.github.com>
2022-05-25 22:04:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const client = Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 client.on('ready', () => {
     console.log(`${client.user.username} is ready!`);
 })
+client.on('message', message => {
+    if(message.content === 'Hello'){
+       message.reply('Hi bro');
+        }
+})
+client.login(process.env.TOKEN)
