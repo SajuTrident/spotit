f1ca663c972435cbc25657fb80a4568832c3f338
Sajad pp <96676224+SajuTrident@users.noreply.github.com>
2022-05-25 21:59:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const Discord = require('discord.js');
+const client = Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
+client.on('ready', () => {
+    console.log(`${client.user.username} is ready!`);
+})
